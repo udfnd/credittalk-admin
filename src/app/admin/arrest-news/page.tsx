444665d64ccfb9f211ec41ed63ad3f9a2948603e
udfnd <hsm9300@naver.com>
2025-06-09 06:26:30 +0900

    20250609

--- src/app/admin/arrest-news/page.tsx
+++ src/app/admin/arrest-news/page.tsx
@@ -3,7 +3,6 @@
 
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 
 interface ArrestNews {
   id: number;
